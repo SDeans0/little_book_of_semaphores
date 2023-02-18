6139e30f0b477594035f238a11310936f87a5916
Sam Deans <samuel.deans@jic.ac.uk>
2023-02-18 15:15:00 +0000

    fix: safely increment/decrement in barrier

--- chapters/ch_3/code/barrier.py
+++ chapters/ch_3/code/barrier.py
@@ -2,18 +2,23 @@
 import threading
 
 class Rendezvous:
-    def __init__(self, n_threads: int) -> None:
+    def __init__(self, n_threads: int, mutex: threading.Semaphore) -> None:
         self.count = 0
         self.n_threads = n_threads
+        self.mutex = mutex
     
     def reached(self) -> bool:
         return self.count == self.n_threads
     
     def inc(self):
+        self.mutex.acquire()
         self.count += 1
+        self.mutex.release()
     
     def dec(self):
+        self.mutex.acquire()
         self.count -= 1
+        self.mutex.release()
 
 def wait_for_critical(sem: threading.Semaphore, rv: Rendezvous, thread_id: int):
     print(f"Started {thread_id}")
@@ -27,7 +32,8 @@ def wait_for_critical(sem: threading.Semaphore, rv: Rendezvous, thread_id: int):
 
 def main(n_threads = 10):
     sem = threading.Semaphore(0)
-    rv = Rendezvous(n_threads)
+    mutex = threading.Semaphore(1)
+    rv = Rendezvous(n_threads, mutex)
     threads = []
     for i in range(n_threads):
         new_thread = threading.Thread(target=wait_for_critical, args=(sem, rv, i))
